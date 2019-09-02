f6382f98e553c0899206d70dfa32de038a8bfe7a
kvoyant <kvoyant@gmail.com>
2019-09-03 01:24:48 +0900

    버튼 클릭시 원화 달러 계산 로직 완료

--- MoneyConverter/ViewController.swift
+++ MoneyConverter/ViewController.swift
@@ -10,11 +10,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var currencySegment: UISegmentedControl!
+    @IBOutlet weak var sourceMoneyField: UITextField!
+    @IBOutlet weak var targetMoneyLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func convertMoney(_ sender: Any) {
+        let ratio : Double
+        switch currencySegment.selectedSegmentIndex {
+        case 0:
+            ratio = 0.00085
+        case 1:
+            ratio = 1178.5
+        default:
+            ratio = 1.0
+        }
+        
+        let targetMoneyString:String
+        if let sourceMoney = Double(sourceMoneyField.text!) {
+            targetMoneyString = "\(sourceMoney * ratio)"
+        } else {
+            targetMoneyString = "Error"
+        }
+        
+        targetMoneyLabel.text = targetMoneyString
+    }
 }
 
